79866862cc77213e4163360754411d527676fe94
Mats Holmberg <mats.holmberg@artflow.fi>
2025-02-13 08:49:41 +0200

    mimelist modified

--- xorg/wacom_intuos4_setup.sh
+++ xorg/wacom_intuos4_setup.sh
@@ -6,7 +6,7 @@
 #xsetwacom list
 
 # Map stylus to screen (can also use the argument "next")
-xsetwacom set 26 MapToOutput HEAD-0
+xsetwacom set 11 MapToOutput HEAD-0
 
 # Reduce tablet drawing area height (make aspect ratio correct)
 # xsetwacom get stylus Area
@@ -14,4 +14,4 @@ xsetwacom set 26 MapToOutput HEAD-0
 # where height is tablet_width * screen_height / screen_width
 
 # correction for 2560x1440 display
-xsetwacom set 26 Area 0 0 65024 36576
+xsetwacom set 11 Area 0 0 65024 36576
